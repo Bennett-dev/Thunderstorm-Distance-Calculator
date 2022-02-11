72288c29f782d641269e4f1ea68456f5d337e979
Bennett-dev <65304132+Bennett-dev@users.noreply.github.com>
2022-02-11 16:04:47 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+def calc(T, F=60):
+    K = (5/9*(F-32))+273.16
+    VsoundK = 643.855*(K/273.15)**0.5
+    VsoundM = 1.1507794*VsoundK
+    return VsoundM * (T/3600)
+x = int(input("count the seconds between the flash and the sound: "))
+y = int(input("enter air temp: "))
+z = str(round(calc(x, y), 2))
+print("the storm is "+z+" miles away")
+time.sleep(10)
